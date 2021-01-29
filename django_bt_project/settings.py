33f173b666909faf9a7d0a9ff2bf135b84898590
jssrob <jssrob123@gmail.com>
2021-01-28 21:37:21 -0500

    Add files via upload

--- django_bt_project/settings.py
+++ django_bt_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=my)*n^i0suuuq_w_p6w#zgf2730sl#6h11b==5@zrr*)e-6_z'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
