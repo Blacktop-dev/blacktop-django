0db669764ac9da9d213ea12154a8b4dcfc08a4dc
Connor Young <41554364+connory33@users.noreply.github.com>
2021-02-02 23:48:25 -0800

    Add files via upload

--- bt_site/admin.py
+++ bt_site/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from bt_site.models import Player, Group, Venue
+from bt_site.models import Player, Group, Venue, Availability
 
 admin.site.register(Player)
 admin.site.register(Group)
 admin.site.register(Venue)
+admin.site.register(Availability)
