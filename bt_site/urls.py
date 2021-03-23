090840b0fd78524f54499a2406fdeb336fd85b60
jssrob <jssrob123@gmail.com>
2021-03-22 20:31:20 -0400

    Add files via upload

---
+++ bt_site/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'bt_site'
+
+urlpatterns = [
+    url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^display/$', views.Display.as_view(), name='display'),
+    url(r'^friendrequest/$', views.FriendRequestView.as_view(), name='friendrequest'),
+]
